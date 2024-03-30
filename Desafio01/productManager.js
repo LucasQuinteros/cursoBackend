3e577f189d3809673f79fd7652ffa6d2200ba308
Lucas Quinteros <quinteroslucas@gmail.com>
2024-03-30 19:46:04 -0300

    Desafio01 terminado

---
+++ Desafio01/productManager.js
@@ -0,0 +1,50 @@
+class ProductManager{
+    constructor(){
+        this.products = [];
+    }
+    addProduct(title,description,price,thumbnail,code,stock){
+        if (! this.productExists(code)){
+            let newProduct = {
+                id : this.products.length , 
+                title,
+                description ,
+                price  ,
+                thumbnail ,
+                code,
+                stock
+            }
+            if (Object.values(newProduct).includes(undefined)){
+                console.log("Todos los campos son obligatorios");
+                
+            }
+            else this.products.push(newProduct)
+        }
+        else{
+            console.log( `Error el producto ${title} con codigo ${code} ya existe`)
+        }
+        
+    }
+    getProducts(){
+        console.log(this.products);
+        return this.products;
+    }
+    productExists(codeBuscado){
+        return this.products.find((element) => element.code === codeBuscado);
+    }
+    getProductById( idBuscado){
+
+        let p = this.products.find((element) => element.id === idBuscado);
+        if (!p) p = "Error no se encontro el producto";
+        console.log(p);
+        return p;
+    }
+}
+
+// Test
+let p = new ProductManager();
+
+p.addProduct("producto0","Este es un producto prueba",200,"Sin imagen","abc123",25)
+p.addProduct("producto1","Este es un producto prueba",200,"Sin imagen","abc12")
+p.getProducts()
+p.getProductById(5)
+while (true){}
