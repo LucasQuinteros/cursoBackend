eda4760ce8ae775fc4446fabc911c1c99fb8c4b3
Lucas Quinteros <quinteroslucas@gmail.com>
2024-07-06 12:22:10 -0300

    estructura

---
+++ Preentrega03/src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import router from "./routes/index.js";
+import Database from "./config/mongoDb.config.js";
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
+import cookieParser from "cookie-parser";
+import passport from "passport";
+import { initializePassport } from "./config/passport.config.js";
+
+const PORT = 8080;
+const urlDb = 'mongodb+srv://lucasquinteros:cascadas@ecommerce.a8f2glv.mongodb.net/ecommerce'
+const app = express();
+const db = new Database(urlDb)
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : true}));
+app.use(cookieParser("secreto"));
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: urlDb,
+        ttl : 15
+    }),
+    secret : "secretKey",
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+initializePassport()
+app.use("/api",router);
+
+
+app.listen(PORT,() => (console.log(`Server listen on port ${PORT}`)))
---
+++ Preentrega03/src/middleware/isLogin.middleware.js
@@ -0,0 +1,16 @@
+
+
+
+export async function isLogin(req,res,next){
+    if(req.session.user){
+        
+        next()
+    }
+    else{
+        res.status(401).json({status: "Error", msg : "Usuario no logueado"})
+    }
+}
+
+export default{
+    isLogin
+}
